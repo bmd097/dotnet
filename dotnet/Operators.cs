08e307276ec86c1318da8774e90b36eb800562c5
biswamohan_dwari <biswamohan_dwari@EPAM.com>
2024-01-23 17:49:28 +0530

    Operators in c#

---
+++ dotnet/Operators.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet
+{
+    enum OperatorType {
+        ARITHMETIC, RELATIONAL, BOOLEAN, BITWISE, EQUALITY
+    }
+    internal interface Operator
+    {
+        void handle();
+    }
+    class ArithmeticOperators : Operator
+    {
+        public void handle()
+        {
+            Console.WriteLine("1 + 2 = "+ (1+2) );
+            Console.WriteLine("1 * 2 = " + (1 * 2));
+            Console.WriteLine("1 - 2 = " + (1 - 2));
+            Console.WriteLine("1 / 2 = " + (1 / 2));
+            Console.WriteLine("1 % 2 = " + (1 % 2));
+        }
+    }
+    class RelationalOperators : Operator
+    {
+        public void handle()
+        {
+            Console.WriteLine("1 < 2 = " + (1 < 2));
+            Console.WriteLine("1 > 2 = " + (1 > 2));
+            Console.WriteLine("1 <= 2 = " + (1 <= 2));
+            Console.WriteLine("1 >= 2 = " + (1 >= 2));
+            Console.WriteLine("1 != 2 = " + (1 != 2));
+            Console.WriteLine("2 == 2 = " + (1 == 2));
+        }
+    }
+    class BooleanOperators : Operator
+    {
+        public void handle()
+        {
+            Console.WriteLine("true && false = " + (true && false));
+            Console.WriteLine("true && true = " + (true && true));
+            Console.WriteLine("true || false = " + (true || false));
+            Console.WriteLine("true || true = " + (true || true));
+        }
+    }
+    class BitwiseOperators : Operator
+    {
+        public void handle()
+        {
+            Console.WriteLine("1 & 2 = " + (1 & 2));
+            Console.WriteLine("1 | 2 = " + (1 | 2));
+            Console.WriteLine("~2  = " + (~2));
+        }
+    }
+    class EqualityOperators : Operator
+    {
+        public void handle()
+        {
+            Console.WriteLine("1 == 2 = " + (1 == 2));
+            Console.WriteLine("2 == 2 = " + (2 == 2));
+            Console.WriteLine("Hi == Hi"+("Hi" == "Hi"));
+        }
+    }
+}
--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -10,7 +10,19 @@ namespace dotnet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Mohan!");
+            Console.WriteLine("Hello Earth!");
+            Dictionary<OperatorType, Operator> strategies = new Dictionary<OperatorType, Operator>();
+            strategies.Add(OperatorType.ARITHMETIC,new ArithmeticOperators());
+            strategies.Add(OperatorType.RELATIONAL,new RelationalOperators());
+            strategies.Add(OperatorType.BOOLEAN,new BooleanOperators());
+            strategies.Add(OperatorType.BITWISE,new BitwiseOperators());
+            strategies.Add(OperatorType.EQUALITY,new EqualityOperators());
+            foreach (OperatorType operatorType in Enum.GetValues(typeof(OperatorType)))
+                if (strategies.ContainsKey(operatorType))
+                {
+                    Console.WriteLine(operatorType);
+                    strategies[operatorType].handle();
+                }
             Console.ReadLine();
         }
     }
